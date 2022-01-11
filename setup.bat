b4bddc6a815193228cb679b1ad9424f11a0dd1b1
T8889091 <sirotaben@gmail.com>
2022-01-11 14:20:38 +0200

    setup

---
+++ setup.bat
@@ -0,0 +1,7 @@
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+pip install google-auth-oauthlib
+pip install google-api-python-client
+pip install google-auth
+pip install --upgrade google-auth-httplib2
+nothing.bat
